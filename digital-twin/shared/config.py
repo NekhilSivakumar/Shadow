2efa1c61f631be05168ee419f21518483d7b2207
Nekhil Sivakumar <nekhilsivakumar@gmail.com>
2026-09-12 14:23:06 +0530

    Api Update

--- digital-twin/shared/config.py
+++ digital-twin/shared/config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-ANTHROPIC_API_KEY = os.getenv("sk-nCLP1vqkjplB0zSBfiyFmmXJruEHRZxwbmXLirJ3rlWegSJS")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 CONTROL_LAYER_URL = os.getenv("CONTROL_LAYER_URL", "http://localhost:8001")
 BRAIN_PORT = int(os.getenv("BRAIN_PORT", "8000"))
